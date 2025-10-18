e1a5784f095a2365cf9282e40d36e8e13f7ff601
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-18 19:26:24 +0530

    fix

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
 
-console.log('Backend URL:', import.meta.env.VITE_BACKEND_URL);
-
 createRoot(document.getElementById("root")!).render(<App />);
