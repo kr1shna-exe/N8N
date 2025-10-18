515ea504807bcf820dd036c5142217d6d56a5874
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-18 18:09:37 +0530

    Add backend URL logging

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
 
+console.log('Backend URL:', import.meta.env.VITE_BACKEND_URL);
+
 createRoot(document.getElementById("root")!).render(<App />);
